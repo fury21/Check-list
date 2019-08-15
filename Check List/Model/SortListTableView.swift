86ae044c528d3833aed87fe3ff2c53e142c500a1
BYCHKOVSKIY <fury21@mail.ru>
2019-08-15 19:45:54 +0300

    Добавил файл SortListTableView

---
+++ Check List/Model/SortListTableView.swift
@@ -0,0 +1,13 @@
+//
+//  SortListTableView.swift
+//  Check List
+//
+//  Created by Александр Б. on 15/08/2019.
+//  Copyright © 2019 Александр Б. All rights reserved.
+//
+
+import Foundation
+
+func sortList() {
+    
+}
