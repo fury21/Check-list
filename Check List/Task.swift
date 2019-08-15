844d7bbdb398a78523644572363a2401d449fe0f
Viktor Prikolota <VP_ampersand_P@icloud.com>
2019-08-15 15:29:44 +0300

    Добавил класс Task

---
+++ Check List/Task.swift
@@ -0,0 +1,17 @@
+//
+//  Task.swift
+//  Check List
+//
+//  Created by Viktor on 15/08/2019.
+//  Copyright © 2019 Александр Б. All rights reserved.
+//
+
+import UIKit
+
+struct Task {
+    
+    let title: String
+    let taskList: [String]
+    let isDone: Bool
+    
+}
