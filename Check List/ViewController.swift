e17f3c733cd266f9ce30c5925236ea57ff563a4e
BYCHKOVSKIY <fury21@mail.ru>
2019-08-15 12:11:46 +0300

    Initial Commit

---
+++ Check List/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Check List
+//
+//  Created by Александр Б. on 15/08/2019.
+//  Copyright © 2019 Александр Б. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
