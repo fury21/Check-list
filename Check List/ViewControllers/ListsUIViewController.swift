eaa73b095ae5f458232d5ebc42df6fbafc803a32
BYCHKOVSKIY <fury21@mail.ru>
2019-08-18 23:58:27 +0300

    1) Удалил делигаты и лишний код 2) Сделал обновление первого экрана во override func viewWillAppear(_ animated: Bool)

--- Check List/ViewControllers/ListsUIViewController.swift
+++ Check List/ViewControllers/ListsUIViewController.swift
@@ -8,22 +8,23 @@
 
 import UIKit
 
-class ListsUIViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, ReloadData {
+class ListsUIViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
+    @IBOutlet var tableView: UITableView!
     
     @IBAction func newListButton(_ sender: Any) {
         allLists.append(AllLists(listName: "Новый список", items: []))
         tableView.reloadData()
     }
     
-    @IBOutlet var tableView: UITableView!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
     
-    func reloadData() {
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
         tableView.reloadData()
     }
     
@@ -50,73 +51,14 @@ class ListsUIViewController: UIViewController, UITableViewDelegate, UITableViewD
             cell.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
         }
         
-        // cell.listSublabel = ???
         return cell
+    }    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let indexPath = tableView.indexPathForSelectedRow {
+            let taskVC = segue.destination as! TasksUIViewController
+            
+            taskVC.currentIndexPath = indexPath
+        }
     }
-
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.reloadData()
-    }
-
-
-override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-    //        if segue.identifier == "toDetailSegue" {
-    if let indexPath = tableView.indexPathForSelectedRow {
-        let taskVC = segue.destination as! TasksUIViewController
-        
-        
-        taskVC.currentIndexPath = indexPath
-        taskVC.delegate = self
-        //        }
-    }
-}
-
-
-
-
-/*
- // Override to support conditional editing of the table view.
- override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
- // Return false if you do not want the specified item to be editable.
- return true
- }
- */
-
-/*
- // Override to support editing the table view.
- override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
- if editingStyle == .delete {
- // Delete the row from the data source
- tableView.deleteRows(at: [indexPath], with: .fade)
- } else if editingStyle == .insert {
- // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
- }
- }
- */
-
-/*
- // Override to support rearranging the table view.
- override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
- 
- }
- */
-
-/*
- // Override to support conditional rearranging of the table view.
- override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
- // Return false if you do not want the item to be re-orderable.
- return true
- }
- */
-
-/*
- // MARK: - Navigation
- 
- // In a storyboard-based application, you will often want to do a little preparation before navigation
- override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
- // Get the new view controller using segue.destination.
- // Pass the selected object to the new view controller.
- }
- */
-
 }
--- Check List/ViewControllers/TasksUIViewController.swift
+++ Check List/ViewControllers/TasksUIViewController.swift
@@ -15,10 +15,6 @@ enum AlertType {
 }
 
 
-protocol ReloadData {
-    func reloadData()
-}
-
 class TasksUIViewController: UIViewController {
     
     @IBOutlet var tableView: UITableView!
@@ -27,7 +23,7 @@ class TasksUIViewController: UIViewController {
     var currentIndexPath: IndexPath!
     var refresh = UIRefreshControl()
     
-    var delegate: ReloadData?
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -185,7 +181,6 @@ class TasksUIViewController: UIViewController {
             allLists[currentIndexPath.row].items[indexPath.row].isTaskDone.toggle()
             tableView.reloadData()
             
-            delegate?.reloadData()
         }
         
         func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
@@ -203,7 +198,7 @@ class TasksUIViewController: UIViewController {
                                            type: .edit,
                                            index: indexPath.row)
             }
-            action.image = UIImage(named: "Alarm")
+            action.image = UIImage(named: "edit")
             action.backgroundColor = .gray
             
             return action
@@ -220,120 +215,4 @@ class TasksUIViewController: UIViewController {
             
             return action
         }
-        
-        
-//        //Обновление таблицы, сброс значений
-//        @objc private func handleRefresh() {
-//            refresh.endRefreshing()
-//            for item in allLists[currentIndexPath.row].items {
-//                if item.isTaskDone {
-//                    item.isTaskDone.toggle()
-//                }
-//            }
-//            DispatchQueue.main.asyncAfter(deadline: .now()+0.8) {
-//                self.tableView.reloadData()
-//            }
-//        }
-        
-//        private func createAlertController(title: String, message: String,
-//                                           actionTitle: String, type: AlertType, index: Int = 0) {
-//
-//            let alert = UIAlertController(title: title,
-//                                          message: message,
-//                                          preferredStyle: .alert)
-//
-//            // Добавляю первое текстовое поле
-//            alert.addTextField { (textField) in
-//                switch type {
-//                case .add:
-//                    textField.placeholder = "Укажите название"
-//                case .edit:
-//                    textField.text = allLists[self.currentIndexPath.row].items[index].taskName
-//                }
-//            }
-//
-//            // Добавляю второе текстовое поле
-//            alert.addTextField { (textField) in
-//                switch type {
-//                case .add:
-//                    textField.placeholder = "Укажите количество"
-//                case .edit:
-//                    textField.text = String(allLists[self.currentIndexPath.row].items[index].tasksCount)
-//                }
-//            }
-//
-//            let action = UIAlertAction(title: actionTitle, style: .default) { (action) in
-//                if let taskNameTextField =  alert.textFields?.first?.text,
-//                    !taskNameTextField.isEmpty, let taskCountTextField = alert.textFields?[1].text,
-//                    let taskCount = Int(taskCountTextField) {
-//
-//                    let newTaskList = Tasks(taskName: taskNameTextField, tasksCount: taskCount)
-//                    switch type {
-//                    case .add:
-//                        allLists[self.currentIndexPath.row].items.append(newTaskList)
-//                    case .edit:
-//                        allLists[self.currentIndexPath.row].items[index] = newTaskList
-//                    }
-//                    self.tableView.reloadData()
-//                } else {
-//                    let alert = DefaultAlert.createDefaultAlert(title: "Ошибка", message: "Некорректный ввод")
-//                    self.present(alert, animated: true, completion: nil)
-//                }
-//            }
-//
-//            alert.view.tintColor = #colorLiteral(red: 1, green: 0.8196527362, blue: 0.4653458595, alpha: 1)
-//            let cancelAction = UIAlertAction(title: "Отмена", style: .cancel, handler: nil)
-//            alert.addAction(action)
-//            alert.addAction(cancelAction)
-//            present(alert, animated: true, completion: nil)
-//
-//        }
-        
-        
-        
-        /*
-         // Override to support conditional editing of the table view.
-         override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-         // Return false if you do not want the specified item to be editable.
-         return true
-         }
-         */
-        
-        /*
-         // Override to support editing the table view.
-         override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-         if editingStyle == .delete {
-         // Delete the row from the data source
-         tableView.deleteRows(at: [indexPath], with: .fade)
-         } else if editingStyle == .insert {
-         // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-         }
-         }
-         */
-        
-        /*
-         // Override to support rearranging the table view.
-         override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-         
-         }
-         */
-        
-        /*
-         // Override to support conditional rearranging of the table view.
-         override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-         // Return false if you do not want the item to be re-orderable.
-         return true
-         }
-         */
-        
-        /*
-         // MARK: - Navigation
-         
-         // In a storyboard-based application, you will often want to do a little preparation before navigation
-         override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-         // Get the new view controller using segue.destination.
-         // Pass the selected object to the new view controller.
-         }
-         */
-        
 }
